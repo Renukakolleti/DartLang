c410b8f88ccb5085d662ac3e5c679ab27ee73556
renuka <renuka.kolleti@imaginnovate.com>
2023-12-15 12:29:37 +0530

    first commit

---
+++ firstCode.dart
@@ -0,0 +1,41 @@
+void main() {
+  
+  // My first output.
+  print("Hello World"); 
+  
+  print("This is my first application");     // second output comment
+  
+  print(4);      // integer value
+  
+  print(2 + 2);   // expresstions
+  
+  print(true);     // boolean values
+  
+  int score = 23;
+	var count = 23;     // It is inferred as integer automatically by compiler
+	int hexValue = 0xEADEBAEE;
+
+	// Numbers: double
+	double percentage = 93.4;
+	var percent = 82.533;
+	double exponents = 1.42e5; 
+
+	// Strings
+	String name = "Henry";
+	var company = "Google";
+
+	// Boolean
+	bool isValid = true;
+	var isAlive = false;
+  
+  print(score);
+  print(hexValue);
+  print(percentage);
+  print(percent);
+  print(name);
+  print(company);
+  print(isValid);
+  print(isAlive);
+	print(count);
+	print(exponents);
+}
