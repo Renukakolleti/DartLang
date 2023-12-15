86bcc7a0f58a6a7d9e22332a9d4bfacaf050f61c
renuka <renuka.kolleti@imaginnovate.com>
2023-12-15 14:52:07 +0530

    string interpolation

---
+++ string_string_interpolation.dart
@@ -0,0 +1,29 @@
+void main() {
+  var isCool = true;
+  int x = 2;
+  "John";
+  4.5;
+
+  String s1 = 'single';
+  String s2 = "Double";
+  String s3 = "It\'s easy";
+  String s4 = "It's easy";
+
+  String s5 = 'This is going to be a very long String. '
+      'This is just a sample demo programming language.';
+
+  String name = "Kevin";
+
+  print("My name is $name");
+  print("$isCool");
+  print("$x");
+
+  print("$s1 $s2 $s3 $s4 ");
+  print("$s5");
+
+  int l = 20;
+  int b = 10;
+
+  print("The length and breadth is ${l + b}");
+  print("The Area of rectangle of length $l and breadth $b is ${l * b}");
+}
